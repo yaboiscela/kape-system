4c2e6e3edee57bba6765e70d41e39385fc99e443
yaboiscela <88175376+yaboiscela@users.noreply.github.com>
2025-10-08 16:09:42 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://kape-system-backend.onrender.com',          // ✅ Add this line
+      '/api': 'https://kape-backend-1r4o.onrender.com',          // ✅ Add this line
     },
   },
   plugins: [
